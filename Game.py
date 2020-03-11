4944958d9e8e51a9a19a8291389e7eb93b8dbb26
Иван Дорофеев <axxel123@rambler.ru>
2020-03-11 15:30:48 +0300

    Диплом

---
+++ Game.py
@@ -0,0 +1,17 @@
+
+
+class Dron:
+
+    def __init__(self):
+
+    def __str__(self):
+
+    def turn_to(obj): #повернуться к объекту/точке
+
+    def move_at(obj): #двигаться к объекту/точке
+
+    def stop(): #остановиться
+
+    def load_from(obj): #загрузить элериум от объекта в трюм
+
+    def unload_to(obj): #разгрузить элериум из трюма в объект
